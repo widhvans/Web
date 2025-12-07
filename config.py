af8a66ce5afe19b49b6d70b5c9abbd89ed9ed16d
widhvans <110589162+widhvans@users.noreply.github.com>
2025-12-07 10:37:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import os
 
+class Config:
+    # Telegram Bot Token (Get from @BotFather)
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8368477494:AAEVLeVK20P3xZUTSWbarOuqeTs_mgGitXE")
+    
+    # Groq API Key
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
+    
+    # Your Koyeb App URL
+    WEBAPP_URL = "https://oral-melonie-as772685-9f0b873d.koyeb.app/"
+    
+    # Server Port (Koyeb uses 8000 by default)
+    PORT = int(os.environ.get("PORT", 8080))
+  
